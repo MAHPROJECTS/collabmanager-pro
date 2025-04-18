5c31eb3267d1114e99ee6c7e1108cb71cafdf1cf
hum-projects <humaira2011ahmed@gmail.com>
2025-04-18 17:59:19 +0100

    i added code

--- src/tasks.py
+++ src/tasks.py
@@ -1,5 +1,6 @@
 def create_task():
-    pass  # TODO: Add task creation logic
+    task_name = input("Task name: ")
+    print(f"Task received: {task_name}")  # TODO: Add task creation logic
 
 def update_task():
     pass  # TODO: Add task update logic
