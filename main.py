58709657e499e56887227fe6b3487f6c64901deb
hum-projects <humaira2011ahmed@gmail.com>
2025-04-18 18:11:01 +0100

    working

--- main.py
+++ main.py
@@ -16,21 +16,36 @@ def main():
         choice = input("Select an option: ")
 
         if choice == '1':
+            print("         ")
+            print("Register:")
             auth.register_user()
         elif choice == '2':
+            print("         ")
+            print("Login:")
             auth.login_user()
         elif choice == '3':
+            print("         ")
+            print("Create Project:")
             projects.create_project()
         elif choice == '4':
+            print("         ")
+            print("Join Project:")
             projects.join_project()
         elif choice == '5':
-            print("Create Task")
+            print("         ")
+            print("Create Task:")
             tasks.create_task()
         elif choice == '6':
+            print("         ")
+            print("Update Task:")
             tasks.update_task()
         elif choice == '7':
+            print("         ")
+            print("View Tasks:")
             tasks.list_tasks()
         elif choice == '8':
+            print("         ")
+            ("Export Report:")
             reporting.export_tasks_to_csv()
         elif choice == '9':
             break
